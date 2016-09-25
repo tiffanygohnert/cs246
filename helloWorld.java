bd6d5f4a75dc5e3d3692813dff68cce27c0ec0d5
Tiffany Gohnert <tiffanygohnert@gmail.com>
2016-09-24 21:53:59 -0700

    added helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,18 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author whi08_000
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
